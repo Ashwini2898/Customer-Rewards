9f4dac5cd30e73e85c4bea0f6b78329cbfca16cc
Ashwini2898 <ashwini281998@gmail.com>
2025-07-31 11:00:51 +0530

    Added customer and transaction files

---
+++ src/main/java/com/rewardsystem/rewardmanager/Entity/Customer.java
@@ -0,0 +1,57 @@
+package com.rewardsystem.rewardmanager.Entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="customer_master")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+	private Integer custId;
+	
+	@NotNull
+	@NotBlank(message="Please enter your name")
+	@Size(min=2, max=30,message=" Name should have atleast 2 characters")
+   @Column(name = "cust_name")
+	private String custName;
+	
+	@NotNull
+	@NotBlank(message="Please enter your phone number")
+	@Pattern(regexp="([7-9][0-9]{9})")
+    @Column(name = "cust_mobile")
+	private String custMobile;
+	
+	@NotNull
+	@NotBlank(message="Please enter amount spent")
+	 @Column(name = "total_spent")
+		private Double totalSpent;
+	 
+	 @Column(name = "reward_points")
+		private Integer rewardPoints;
+	 
+	 @OneToMany(mappedBy = "customer")
+	 private List<Transaction> transaction;
+	 
+	 
+
+}
